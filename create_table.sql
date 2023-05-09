89a46bbad8c3bd4e0975088f6fba6a8b4f510f48
Helen <70480275+krykova3ea@users.noreply.github.com>
2023-05-10 00:01:20 +0300

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Strings_26
+( stroka VARCHAR2(4000)
+);
+INSERT INTO Strings_26
+VALUES ('Cabjrehwklhgfjrehwkyupolkoraclenmoracleppmicrosoftmmicrosoftumicrosoft');
+INSERT INTO Strings_26
+VALUES ('yyyyyyyy');
+INSERT INTO Strings_26
+VALUES ('grant');
+INSERT INTO Strings_26
+VALUES ('');
